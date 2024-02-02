ce00e10904e322622d0b4899ebca5e3121747bf9
Muhtasim Ibteda Shochcho <sho25100@gmail.com>
2024-02-03 01:17:23 +0600

    P

---
+++ Part 3/polymorphism.py
@@ -0,0 +1,22 @@
+'''
+Polymorphism ---> Method Overloading and method overriding 
+'''
+
+class Parent:
+
+    def __init__(self,num):
+        self.__num = num
+
+    def get_num(self):
+        return self.__num
+    
+class Child(Parent):
+
+    def show(self):
+        print("This is in child class")
+
+son = Child(100)
+
+print(son.get_num())
+son.show()
+
