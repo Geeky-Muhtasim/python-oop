50a4d087e99fef08d217a40268c7f09010c54afc
Muhtasim Ibteda Shochcho <sho25100@gmail.com>
2024-02-03 01:38:14 +0600

    P

---
+++ Part 3/super_exp2.py
@@ -0,0 +1,22 @@
+class Phone:
+
+    def __init__(self,price,brand,camera):
+        print("Inside phone constructor")
+        self.price = price
+        self.brand = brand
+        self.camera = camera
+
+class SmarterPhone(Phone):
+
+    def __init__(self,price,brand,camera, os, ram):
+        print("First in child")
+        super().__init__(price,brand,camera)
+        self.os = os
+        self.ram = ram
+        print("Inside smartphone constructor")
+
+s = SmarterPhone(40000, "Xiaomi", 50, "Android", 8)
+
+print(s.os)
+
+print(s.brand)
