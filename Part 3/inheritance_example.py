1540ebafa857bec141e06c275975fcaec0ba42f9
Muhtasim Ibteda Shochcho <sho25100@gmail.com>
2024-02-03 01:11:42 +0600

    P

--- Part 3/inheritance_example.py
+++ Part 3/inheritance_example.py
@@ -1,12 +1,17 @@
 class Phone:
     def __init__(self,price,brand,camera):
         print("Inside the constructor")
-        self.price = price
-        self.__brand = brand
+        self.__price = price
+        self.brand = brand
         self.camera = camera
 
+    def buy(self):
+        print("Buying the phone")
+
 class Smartphone(Phone):
-    pass
+    
+    def buy(self):
+        print("Buying the smartphone")
 
 '''
 Child class can acquire the constructor of Parent class.
@@ -16,3 +21,12 @@ But it can't access the private attributes of parent class.
 
 s = Smartphone(20000, 'Samsung', 20)
 print(s.brand,s.camera)
+
+'''
+Here, the buy function will execute the method
+of the smartphone class. As s refers to the object of Smartphone
+class. So, it shall execute the buy method from its own class.
+This is also called method overriding.
+'''
+
+s.buy()
