3403934161af958fbf869fd4c1724edcd3da26aa
Muhtasim Ibteda Shochcho <sho25100@gmail.com>
2024-02-03 00:59:26 +0600

    P

---
+++ Part 3/inheritance.py
@@ -0,0 +1,21 @@
+class User:
+
+    def login(self):
+        print("Login")
+    def register(self):
+        print("Register")
+
+class Student(User):  #Here, student is the subclass of User class
+
+    def enroll(self):
+        print("Enroll")
+    
+    def review(self):
+        print("Review")
+
+std1 = Student()
+
+std1.login()
+std1.register()
+std1.enroll()
+std1.review()
